a1800d262fad1abc2042107cf8f06bf2ed1d3a11
Hiroto Kawabata <13412823+hkawabata@users.noreply.github.com>
2022-08-19 16:44:49 +0900

    update

--- scala_practice/src/main/scala/jp/hkawabata/parser_combinator/MyApp.scala
+++ scala_practice/src/main/scala/jp/hkawabata/parser_combinator/MyApp.scala
@@ -1,7 +1,40 @@
 package jp.hkawabata.parser_combinator
 
+import scala.util.parsing.combinator._
+
 object MyApp {
   def main(args: Array[String]): Unit = {
-    MyParser("I LOVE MUSIC")
+    MyTokenParser("I LOVE MUSIC")
+    MyTokenParser("I LIKE MUSIC")
+    println(MyRegexParser("Xx1234"))
+    println(MyRegexParser("1234Xx"))
+  }
+}
+
+object MyTokenParser extends JavaTokenParsers {
+  def third = s ~ v ~ o ^^ { case subj ~ verb ~ obj => println(s"$subj + $verb + $obj") }
+  def s = "I" | "WE"
+  def v = "LOVE"
+  def o = "YOU" | "MUSIC"
+
+  def apply(s : String) : Unit = {
+    parseAll(third, s) match {
+      case Success(_, _) => println("Success")
+      case x => println("Failure: " + x)
+    }
+  }
+}
+
+object MyRegexParser extends RegexParsers {
+  def p1 = """[a-zA-Z]+""".r
+  def p2 = """\d+""".r
+
+  def word: Parser[String] = p1 ~ p2 ^^ { case x ~ y =>  x + ":" + y }
+
+  def apply(s: String): String = {
+    parseAll(word, s) match {
+      case Success(x, y) => x
+      case x => "Failure: " + x
+    }
   }
 }
